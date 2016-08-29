9430389052def09101cfbfc7631227782e180afd
Soutaro Matsumoto <matsumoto@soutaro.com>
2016-08-29 14:58:52 +0900

    Fix test

--- test/cycromatic_test.rb
+++ test/cycromatic_test.rb
@@ -1,11 +1,7 @@
-require 'test_helper'
+require_relative 'test_helper'
 
 class CycromaticTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Cycromatic::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
